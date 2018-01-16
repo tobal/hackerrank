ff8109da6f1221eebb9215f15380a8b2ea6bbd05
Tobal <tobal17@gmail.com>
2018-01-16 16:12:03 +0100

    algo/warmup: add solution for A Very Big Sum
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ algorithms/warmup/a-very-big-sum.scala
@@ -0,0 +1,15 @@
+object Solution {
+
+    def aVeryBigSum(n: Int, ar: Array[Long]): Long = ar.sum
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in)
+        var n = sc.nextInt()
+        var ar = new Array[Long](n)
+        for(ar_i <- 0 to n-1) {
+           ar(ar_i) = sc.nextLong()
+        }
+        val result = aVeryBigSum(n, ar)
+        println(result)
+    }
+}
