2e06f7918a9e5bea39f7a82e1e57a61674a1fb26
Tobal <tobal17@gmail.com>
2018-01-23 15:17:23 +0100

    func-prog/intro: full solution of Perimeter of Polygon
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/introduction/perimeter-of-polygon.scala
+++ functional-programming/introduction/perimeter-of-polygon.scala
@@ -2,6 +2,11 @@ object Solution {
     def lengthOf(pointA: (Int, Int), pointB: (Int, Int)): Double = {
         scala.math.sqrt(scala.math.pow(pointA._1 - pointB._1, 2) + scala.math.pow(pointA._2 - pointB._2, 2))
     }
+    
+    def foldPerimeter(acc: Double, prevPoint: (Int, Int), points: List[(Int, Int)]): Double = points match {
+        case Nil => acc
+        case next :: rest => foldPerimeter(acc + lengthOf(prevPoint, next), next, rest)
+    }
 
     def main(args: Array[String]) {
         val sc = new java.util.Scanner (System.in)
@@ -10,7 +15,6 @@ object Solution {
         for(arr_i <- 0 to case_n - 1) {
             arr(arr_i) = (sc.nextInt(), sc.nextInt())
         }
-        println(lengthOf(arr.head, arr.head))
-        //println( arr.foldLeft(0.0: Double, (arr.head): (Int, Int))( (perim, prevPoint, newPoint) =>  )
+        println(foldPerimeter(0.0, arr.last, arr.toList))
     }
 }
