c0753198048ca43a06f6295e9327d81758b40b1b
Tobal <tobal17@gmail.com>
2018-01-25 16:00:04 +0100

    algo/warmup: right solution of Birthday Cake Candles
    Signed-off-by: Tobal <tobal17@gmail.com>

--- algorithms/warmup/birthday-cake-candles.scala
+++ algorithms/warmup/birthday-cake-candles.scala
@@ -1,8 +1,16 @@
 object Solution {
 
-    def birthdayCakeCandles(n: Int, ar: Array[Int]): Int =  {
-        val l = ar.toList
-        l.filter(_ == l.max).size
+    def countMax(count: Int, max: Int, ar: List[Int]): Int = ar match {
+        case Nil => count
+        case next :: rest => {
+            if (next > max) countMax(1, next, rest)
+            else if (next == max) countMax(count + 1, max, rest)
+            else countMax(count, max, rest)
+        }
+    }
+
+    def birthdayCakeCandles(n: Int, ar: Array[Int]): Int = {
+        countMax(0, 0, ar.toList)
     }
 
     def main(args: Array[String]) {
