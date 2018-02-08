7d8c757a2bc78274a1f5a061b008a484c3075019
Tobal <tobal17@gmail.com>
2018-02-08 17:20:20 +0100

    func-prog/recursion/sierpinski: just draw a basic triangle
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/sierpinski/Solution.scala
+++ functional-programming/recursion/sierpinski/Solution.scala
@@ -1,13 +1,27 @@
 #!/usr/bin/env scala
 
 object Solution {
+  val width = 63
+  val height = 32
+
+  def inTriangle(row: Int, column: Int): Boolean = {
+    if(column < width / 2 + row + 1 &&
+      column > width / 2 - row + 1) true
+    else false
+  }
+
   def drawTriangles(n: Int) {
-    println(n)
-    //Draw the N'th iteration of the fractal as described 
-    // in the problem statement
+    for ( row <- 1 to height ) yield {
+      for ( column <- 1 to width ) yield {
+        if(inTriangle(row, column)) print("1")
+        else print("_")
+      }
+      println("")
+    }
   }
 
   def main(args: Array[String]) {
-    drawTriangles(readInt())
+    //drawTriangles(readInt())
+    drawTriangles(1)
   }
 }
