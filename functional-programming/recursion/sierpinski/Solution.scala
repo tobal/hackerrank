e9dc3bfb9eb371c14b0c3c1eacb4a51c4698f5db
Tobal <tobal17@gmail.com>
2018-02-08 16:36:30 +0100

    func-prog/recursion/sierpinski: add initial, skeleton solution script
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ functional-programming/recursion/sierpinski/Solution.scala
@@ -0,0 +1,13 @@
+#!/usr/bin/env scala
+
+object Solution {
+  def drawTriangles(n: Int) {
+    println(n)
+    //Draw the N'th iteration of the fractal as described 
+    // in the problem statement
+  }
+
+  def main(args: Array[String]) {
+    drawTriangles(readInt())
+  }
+}
