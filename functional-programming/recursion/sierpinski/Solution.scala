cdeede43be5d73df93bafd35f658a1d2ef9ea662
Tobal <tobal17@gmail.com>
2018-02-09 14:34:05 +0100

    func-prog/recursion/sierpinski: print first iteration statically
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/sierpinski/Solution.scala
+++ functional-programming/recursion/sierpinski/Solution.scala
@@ -3,10 +3,22 @@
 object Solution {
   val width = 63
   val height = 32
+  //val width = 7
+  //val height = 4
 
   def inTriangle(row: Int, column: Int): Boolean = {
     if(column < width / 2 + row + 1 &&
-      column > width / 2 - row + 1) true
+      column > width / 2 - row + 1) {
+        if (row > height / 2) {
+          if (column < width / 4 + row + 1 - height / 2 &&
+              column > width / 4 - row + 1 - height / 2
+              ||
+              column < (width / 4 + width / 2) + row + 2 + height / 2 &&
+              column > (width / 4 + width / 2) - row + 2 + height / 2) true
+          else false
+        }
+        else true
+      }
     else false
   }
 
