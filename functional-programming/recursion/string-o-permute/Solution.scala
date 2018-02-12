0a226c6c3d7d5d3c759a6c26c1aea8c2dd051e45
Tobal <tobal17@gmail.com>
2018-02-12 10:39:41 +0100

    func-prog/recursion/string-o-permute: the most concise solution
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/string-o-permute/Solution.scala
+++ functional-programming/recursion/string-o-permute/Solution.scala
@@ -1,18 +1,10 @@
 #!/usr/bin/env scala
 
 object Solution {
-    def perm(s: String): String = {
-        s.sliding(2, 2)
-          .toList
-          .flatMap(_.reverse)
-          .mkString("")
-    }
-
     def main(args: Array[String]) {
         val cases = readInt()
         for (i <- 0 to cases) {
-            val s = readLine()
-            println(perm(s))
+            println(readLine().grouped(2).toList.flatMap(_.reverse).mkString)
         }
     }
 }
