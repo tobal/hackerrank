ecfc1c299333340a384e299c99068c9f100cf49e
Tobal <tobal17@gmail.com>
2018-02-12 10:34:27 +0100

    func-prog/recursion/string-o-permute: right solution
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/string-o-permute/Solution.scala
+++ functional-programming/recursion/string-o-permute/Solution.scala
@@ -1,17 +1,18 @@
 #!/usr/bin/env scala
 
 object Solution {
-    def switchChars(input: List[Char], output: List[Char]): List[Char] = input match {
-        case x1 :: x2 :: xs => switchChars(xs, output ::: List(x2, x1))
-        case x :: xs => output ::: List(x)
-        case Nil => output
+    def perm(s: String): String = {
+        s.sliding(2, 2)
+          .toList
+          .flatMap(_.reverse)
+          .mkString("")
     }
 
     def main(args: Array[String]) {
         val cases = readInt()
         for (i <- 0 to cases) {
-            val s: List[Char] = readLine().toList
-            println(switchChars(s, List()).mkString)
+            val s = readLine()
+            println(perm(s))
         }
     }
 }
