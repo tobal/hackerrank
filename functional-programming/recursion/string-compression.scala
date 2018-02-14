0c03c3336f34c9d48759558bdb91daed5163a886
Tobal <tobal17@gmail.com>
2018-02-14 11:54:21 +0100

    func-prog/recursion: String Compression solution without concatenation
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/string-compression.scala
+++ functional-programming/recursion/string-compression.scala
@@ -1,22 +1,24 @@
 object Solution {
+    def asChar(num: Int): Char = (num + '0').toChar
+    
     def compress(s: String): String = {
         def rec(input: List[Char], acc: List[Char], prev: Char, occurrances: Int): List[Char] = input match {
             case x :: xs => {
                 if (x == prev) rec(xs, acc, x, occurrances + 1)
-                else if (occurrances > 1) rec(xs, acc ++ List((occurrances + '0').toChar, x), x, 1)
-                else rec(xs, acc ++ List(x), x, 1)
+                else if (occurrances > 1) rec(xs, x :: asChar(occurrances) :: acc, x, 1)
+                else rec(xs, x :: acc, x, 1)
             }
             case x :: Nil => {
-                if (occurrances > 1) acc ++ List((occurrances + '0').toChar)
-                else acc ++ List(x)
+                if (occurrances > 1) asChar(occurrances) :: acc
+                else x :: acc
             }
             case Nil => {
-                if (occurrances > 1) acc ++ List((occurrances + '0').toChar)
+                if (occurrances > 1) asChar(occurrances) :: acc
                 else acc
             }
         }
         val strAsList: List[Char] = s.toList
-        rec(strAsList.tail, List(strAsList.head), strAsList.head, 1).mkString
+        rec(strAsList.tail, List(strAsList.head), strAsList.head, 1).mkString.reverse
     }
 
     def main(args: Array[String]) {
