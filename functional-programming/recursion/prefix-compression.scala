c181887145ebbbbbb7977b559e58713c4109e30c
Tobal <tobal17@gmail.com>
2018-02-16 16:54:59 +0100

    func-prog/recursion: correct solution for Prefix Compression
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/prefix-compression.scala
+++ functional-programming/recursion/prefix-compression.scala
@@ -2,8 +2,7 @@ object Solution {
     def main(args: Array[String]) {
         val x = readLine
         val y = readLine
-        val l = for { i <- 0 to x.size } yield { x.splitAt(i) }
-        val prefix = l.unzip._1.takeWhile(y.startsWith(_)).takeRight(1)(0)
+        val prefix = x zip y takeWhile { case(x, y) => x == y } map (_._1) mkString
         println(s"${prefix.size} $prefix")
         println(s"${x.size - prefix.size} ${x.splitAt(prefix.size)._2}")
         println(s"${y.size - prefix.size} ${y.splitAt(prefix.size)._2}")
