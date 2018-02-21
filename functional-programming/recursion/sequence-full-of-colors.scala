1f6348168af2857aecfde2b0dbd49704d37564ef
Tobal <tobal17@gmail.com>
2018-02-21 14:43:12 +0100

    func-prog/recursion/sequence-full-of-colors: correct solution
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/recursion/sequence-full-of-colors.scala
+++ functional-programming/recursion/sequence-full-of-colors.scala
@@ -1,26 +1,33 @@
 object Solution {
-    def colorNum(colors: String, color: Char): Int = colors.filter(_ == color).size
-    
-    def colorDiff(colors: String, color1: Char, color2: Char): Int = {
-        math.abs(colorNum(colors, color1) - colorNum(colors, color2))
+    case class ColorCounts(red: Int = 0, green: Int = 0, blue: Int = 0, yellow: Int = 0)
+
+    def countColor(ch: Char, counts: ColorCounts): ColorCounts = ch match {
+        case 'R' => counts.copy(red = counts.red + 1)
+        case 'G' => counts.copy(green = counts.green + 1)
+        case 'B' => counts.copy(blue = counts.blue + 1)
+        case 'Y' => counts.copy(yellow = counts.yellow + 1)
     }
-    
-    def checkPrefixes(colors: String): Boolean = {
-        if (colors.init.isEmpty) true
-        else if (colorDiff(colors, 'R', 'G') > 1 || colorDiff(colors, 'Y', 'B') > 1) false
-        else checkPrefixes(colors.init)
+
+    def isValidPrefix(c: ColorCounts): Boolean = (math.abs(c.red - c.green) <= 1 && math.abs(c.blue - c.yellow) <= 1)
+
+    def checkPrefixes(colors: List[Char], colorCounts: ColorCounts): (Boolean, ColorCounts) = colors match {
+        case x :: rest => {
+            val counts = countColor(x, colorCounts)
+            if (isValidPrefix(counts)) checkPrefixes(rest, counts)
+            else (false, counts)
+        }
+        case Nil => (true, colorCounts)
     }
-    
-    def isFullOfColors(colors: String): Boolean = {
-        if (colorNum(colors, 'R') != colorNum(colors, 'G')) return false
-        if (colorNum(colors, 'Y') != colorNum(colors, 'B')) return false
-        checkPrefixes(colors.init)
+
+    def isFullOfColors(colors: List[Char]): Boolean = {
+        val (prefixesOk, counts) = checkPrefixes(colors, ColorCounts())
+        (counts.red == counts.green && counts.blue == counts.yellow && prefixesOk)
     }
 
     def main(args: Array[String]) {
         val cases = readInt
         for (i <- 0 until cases) {
-            if (isFullOfColors(readLine)) println("True")
+            if (isFullOfColors(readLine.toList)) println("True")
             else println("False")
         }
     }
