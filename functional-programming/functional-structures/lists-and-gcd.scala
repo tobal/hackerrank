9fea5339162cd082f52b79a8670a176b80deee49
Tobal <tobal17@gmail.com>
2018-03-08 13:34:59 +0100

    func-prog/func-struct: Lists and GCD complete solution
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/functional-structures/lists-and-gcd.scala
+++ functional-programming/functional-structures/lists-and-gcd.scala
@@ -1,6 +1,6 @@
 object Solution {
     import scala.collection.mutable.ListBuffer
-    
+
     val buff: ListBuffer[List[Tuple2[Int, Int]]] = ListBuffer()
 
     def main(args: Array[String]) {
@@ -14,9 +14,7 @@ object Solution {
         }
         val counted = merged.groupBy(_._1).mapValues(_.map(_._2).toList).toSeq
         val relevant = counted.filter(_._2.size == cases)
-        for ((value, List(_, exponent)) <- relevant) {
-            println(value, exponent)
-        }
-        println(relevant)
+        val results = for ((_, x) <- relevant) yield x.map{ case List(x) => x }.minBy(_._2)
+        println(results.sortBy(_._1).flatMap{ x => x.productIterator.toList }.mkString(" "))
     }
 }
