990f35155b6e1a44e66f0d1934909219eb42be30
Tobal <tobal17@gmail.com>
2018-03-21 16:26:16 +0100

    func-prog/memoization/pentagonal-numbers: complete solution, using Long
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/memoization-and-dp/pentagonal-numbers/pentagonal-numbers.scala
+++ functional-programming/memoization-and-dp/pentagonal-numbers/pentagonal-numbers.scala
@@ -1,9 +1,9 @@
 object Solution {
     import scala.collection.mutable.Map
 
-    val cache: Map[Int, Int] = Map(1 -> 1, 2 -> 5)
+    val cache: Map[Long, Long] = Map(1L -> 1L, 2L -> 5L)
 
-    def pentagonal(num: Int): Int = cache.getOrElseUpdate(num, 2 * num + (num - 2) + pentagonal(num - 1))
+    def pentagonal(num: Long): Long = cache.getOrElseUpdate(num, 2 * num + (num - 2) + pentagonal(num - 1))
 
     def main(args: Array[String]) {
         val cases = readInt
