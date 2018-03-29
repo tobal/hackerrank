d7fd453e0cc2ffab11923497860085dc21b34ab2
Tobal <tobal17@gmail.com>
2018-03-29 14:42:48 +0200

    func-prog/ad-hoc: slightly better solution for Common Divisors
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-programming/ad-hoc/common-divisors.scala
+++ functional-programming/ad-hoc/common-divisors.scala
@@ -1,13 +1,14 @@
 object Solution {
+    def gcd(x: Int, y: Int): Int = if (x == 0) y else gcd(y % x, x)
+
     def divisors(n: Int): List[Int] = for (i <- List.range(1, n+1) if n % i == 0) yield i
 
     def main(args: Array[String]) {
         val sc = new java.util.Scanner (System.in)
         val cases = sc.nextInt()
         val points = for (i <- 0 until cases) {
-            val a = divisors(sc.nextInt()).toSet
-            val b = divisors(sc.nextInt()).toSet
-            println(a.intersect(b).size)
+            val gcdv = gcd(sc.nextInt(), sc.nextInt())
+            println(divisors(gcdv).length)
         }
     }
 }
