058fc472448680f6f1cd6e8e4187ab28891dbc6d
Tobal <tobal17@gmail.com>
2018-04-01 17:21:57 +0200

    artif-int/stat/warmup: add correct mode calculation
    Signed-off-by: Tobal <tobal17@gmail.com>

--- artificial-intelligence/statistics-and-machine-learning/basic-statistics-warmup.scala
+++ artificial-intelligence/statistics-and-machine-learning/basic-statistics-warmup.scala
@@ -6,12 +6,18 @@ object Solution {
         if (s.size % 2 == 0) (lower.last + upper.head) / 2.0 else upper.head
     }
 
+    def calcMode(s: List[Int]): Int = {
+        val bySize = s.groupBy(i => i).mapValues(_.size)
+        val maxCount = bySize.maxBy(_._2)._2
+        bySize.filter(_._2 == maxCount).minBy(_._1)._1
+    }
+
     def main(args: Array[String]) {
         val c = readInt
         val list = readLineAsInts
         val mean: Double = list.sum / list.length.toDouble
         val median: Double = calcMedian(list)
-        val mode = list.groupBy(i => i).mapValues(_.size).maxBy(_._2)._1
+        val mode = calcMode(list)
         //val stdDeviation = 
         //val confIntervalForMean = 
         println(list)
