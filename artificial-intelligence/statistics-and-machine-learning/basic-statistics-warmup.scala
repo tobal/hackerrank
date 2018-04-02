7ee390d08673431f0bc9847f4b616c73c27dda86
Tobal <tobal17@gmail.com>
2018-04-02 20:59:17 +0200

    artif-int/stat/warmup: add standard deviation calculation
    Signed-off-by: Tobal <tobal17@gmail.com>

--- artificial-intelligence/statistics-and-machine-learning/basic-statistics-warmup.scala
+++ artificial-intelligence/statistics-and-machine-learning/basic-statistics-warmup.scala
@@ -1,6 +1,6 @@
 object Solution {
     def readLineAsInts(): List[Int] = readLine.split(" ").toList.map(_.toInt)
-    
+
     def calcMedian(s: List[Int]): Double = {
         val (lower, upper) = s.sortWith(_<_).splitAt(s.size / 2)
         if (s.size % 2 == 0) (lower.last + upper.head) / 2.0 else upper.head
@@ -12,17 +12,32 @@ object Solution {
         bySize.filter(_._2 == maxCount).minBy(_._1)._1
     }
 
+    def calcMeanStd(x: List[Double]): (Double, Double) = {
+
+        @scala.annotation.tailrec
+        def meanStd(x: List[Double], mu: Double, Q: Double, count: Int): (Double, Double) = {
+            if (count >= x.length) (mu, Math.sqrt(Q / x.length))
+            else {
+                val newCount = count + 1
+                val newMu = x(count) / newCount + mu * (1.0 - 1.0 / newCount)
+                val newQ = Q + (x(count) - mu) * (x(count) - newMu)
+                meanStd(x, newMu, newQ, newCount)
+            }
+        }
+
+        meanStd(x, 0.0, 0.0, 0)
+    }
+
     def main(args: Array[String]) {
         val c = readInt
         val list = readLineAsInts
-        val mean: Double = list.sum / list.length.toDouble
+        val (mean, stdDeviation) = calcMeanStd(list.map(_.toDouble))
         val median: Double = calcMedian(list)
         val mode = calcMode(list)
-        //val stdDeviation = 
-        //val confIntervalForMean = 
-        println(list)
+        //val confIntervalForMean =
         println(mean)
         println(median)
         println(mode)
+        println("%.1f".format(stdDeviation))
     }
 }
