584df85010610299829eb614ea2c6682dbc1880e
Tobal <tobal17@gmail.com>
2018-04-13 16:47:47 +0200

    algo/implementation: solution for Breaking the Records
    Signed-off-by: Tobal <tobal17@gmail.com>

--- algorithms/implementation/breaking-the-records.scala
+++ algorithms/implementation/breaking-the-records.scala
@@ -3,7 +3,18 @@ import java.io.PrintWriter
 object Solution {
 
 
-    def breakingRecords(score: Array[Int]): Array[Int] = ???
+    def breakingRecords(score: Array[Int]): Array[Int] = {
+        def maxMin(max: Int, min: Int, maxCount: Int, minCount: Int, score: List[Int]): Array[Int] = score match {
+            case x :: xs if (max < x) => maxMin(x, min, maxCount + 1, minCount, xs)
+            case x :: xs if (min > x) => maxMin(max, x, maxCount, minCount + 1, xs)
+            case x :: xs => maxMin(max, min, maxCount, minCount, xs)
+            case List(x) => if (x > max) Array(maxCount + 1, minCount)
+                            else if (x < min) Array(maxCount, minCount + 1)
+                            else Array(maxCount, minCount) 
+            case Nil => Array(maxCount, minCount)
+        }
+        maxMin(score.toList.head, score.toList.head, 0, 0, score.toList.tail)
+    }
 
     def main(args: Array[String]) {
         val scan = scala.io.StdIn
