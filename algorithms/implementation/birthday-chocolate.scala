e2841c2ec7eedf1264ea8aad9dabc6f104f080da
Tobal <tobal17@gmail.com>
2018-04-16 15:39:41 +0200

    algo/implementation: skeleton of Birthday Chocolate
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ algorithms/implementation/birthday-chocolate.scala
@@ -0,0 +1,13 @@
+object Solution {
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var n = sc.nextInt();
+        var s = new Array[Int](n);
+        for(s_i <- 0 to n-1) {
+           s(s_i) = sc.nextInt();
+        }
+        var d = sc.nextInt();
+        var m = sc.nextInt();
+    }
+}
