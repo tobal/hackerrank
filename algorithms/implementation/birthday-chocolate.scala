fba98aecab0775cb57b374f055a71093809092bb
Tobal <tobal17@gmail.com>
2018-04-18 17:04:31 +0200

    algo/implementation: correct solution of Birthday Chocolate
    Signed-off-by: Tobal <tobal17@gmail.com>

--- algorithms/implementation/birthday-chocolate.scala
+++ algorithms/implementation/birthday-chocolate.scala
@@ -2,12 +2,14 @@ object Solution {
 
     def main(args: Array[String]) {
         val sc = new java.util.Scanner (System.in);
-        var n = sc.nextInt();
+        val n = sc.nextInt();
         var s = new Array[Int](n);
         for(s_i <- 0 to n-1) {
            s(s_i) = sc.nextInt();
         }
-        var d = sc.nextInt();
-        var m = sc.nextInt();
+        val d = sc.nextInt();
+        val m = sc.nextInt();
+
+        println(s.toList.sliding(m,1).toList.map(_.sum).filter(_ == d).size)
     }
 }
