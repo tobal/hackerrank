c78a80a12965ca77aac7a544e0134836075442e0
Tobal <tobal17@gmail.com>
2018-04-24 23:31:24 +0200

    algo/implementation: add skeleton for Between Two Sets
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ algorithms/implementation/between-two-sets.scala
@@ -0,0 +1,19 @@
+import java.io.PrintWriter
+
+object Solution {
+
+    def getTotalX(a: Array[Int], b: Array[Int]): Int = ???
+
+    def main(args: Array[String]) {
+        val scan = scala.io.StdIn
+        val fw = new PrintWriter(sys.env("OUTPUT_PATH"))
+        val nm = scan.readLine.split(" ")
+        val n = nm(0).trim.toInt
+        val m = nm(1).trim.toInt
+        val a = scan.readLine.split(" ").map(_.trim.toInt)
+        val b = scan.readLine.split(" ").map(_.trim.toInt)
+        val total = getTotalX(a, b)
+        fw.println(total)
+        fw.close()
+    }
+}
