79cdb78f11aa3c0309776f4f24b26b05a980e47b
Tobal <tobal17@gmail.com>
2018-05-02 16:56:36 +0200

    algo/implementation: add a bit less retarded solution for Divisible Sum Pairs
    Signed-off-by: Tobal <tobal17@gmail.com>

--- problem-solving/algorithms/implementation/divisible-sum-pairs.scala
+++ problem-solving/algorithms/implementation/divisible-sum-pairs.scala
@@ -1,19 +1,19 @@
 object Solution {
 
-    def divisibleSumPairs(k: Int, ar: List[Int]): Int = {
+    def divisibleSumPairs(k: Int, ar: Array[Int]): Int =  {
         val l = for(x <- ar; y <- ar) yield (x, y)
-        (l.filter{case (x, y) => (x + y) % k == 0}.size) / 2 - 1
+        (l.filter{case (x, y) => (x + y) % k == 0}.filter{case (x, y) => (x != y)}.size) / 2
     }
 
     def main(args: Array[String]) {
-        val sc = new java.util.Scanner (System.in);
-        val n = sc.nextInt();
-        val k = sc.nextInt();
-        var ar = new Array[Int](n);
+        val sc = new java.util.Scanner (System.in)
+        var n = sc.nextInt()
+        var k = sc.nextInt()
+        var ar = new Array[Int](n)
         for(ar_i <- 0 to n-1) {
-           ar(ar_i) = sc.nextInt();
+           ar(ar_i) = sc.nextInt()
         }
-        val result = divisibleSumPairs(n, k, ar)
+        val result = divisibleSumPairs(k, ar)
         println(result)
     }
 }
