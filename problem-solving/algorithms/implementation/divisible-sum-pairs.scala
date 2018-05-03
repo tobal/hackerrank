b764281ac1ebfb8d4fcfbcb65361714b74ea255b
Tobal <tobal17@gmail.com>
2018-05-03 17:11:32 +0200

    algo/implementation/divisible-sum-pairs: styling
    Signed-off-by: Tobal <tobal17@gmail.com>

--- problem-solving/algorithms/implementation/divisible-sum-pairs.scala
+++ problem-solving/algorithms/implementation/divisible-sum-pairs.scala
@@ -1,19 +1,20 @@
 object Solution {
 
-    def divisibleSumPairs(k: Int, ar: Array[Int]): Int =  {
-        val l = for(x <- ar; y <- ar) yield (x, y)
-        (l.filter{case (x, y) => (x + y) % k == 0}.filter{case (x, y) => (x != y)}.size) / 2
+    def divisibleSumPairs(k: Int, ar: List[Int]): Int =  {
+        val pairs = for(x <- ar; y <- ar) yield (x, y)
+        val filtered = pairs.filter{case (x, y) => (x + y) % k == 0}.filter{case (x, y) => (x != y)}
+        (filtered.size) / 2
     }
 
     def main(args: Array[String]) {
         val sc = new java.util.Scanner (System.in)
-        var n = sc.nextInt()
-        var k = sc.nextInt()
-        var ar = new Array[Int](n)
+        val n = sc.nextInt()
+        val k = sc.nextInt()
+        val ar = new Array[Int](n)
         for(ar_i <- 0 to n-1) {
            ar(ar_i) = sc.nextInt()
         }
-        val result = divisibleSumPairs(k, ar)
+        val result = divisibleSumPairs(k, ar.toList)
         println(result)
     }
 }
