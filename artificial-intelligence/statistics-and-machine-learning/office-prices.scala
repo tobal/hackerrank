a9b4d2f49545339759517a7a30280fb433357459
Tobal <tobal17@gmail.com>
2018-05-08 16:55:05 +0200

    artif-int/stat: skeleton for Office Prices
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ artificial-intelligence/statistics-and-machine-learning/office-prices.scala
@@ -0,0 +1,9 @@
+object Solution {
+    def readLineAsDoubles(): List[Double] = readLine.split(" ").toList.map(_.toDouble)
+    
+    def readLineAsInts(): List[Int] = readLine.split(" ").toList.map(_.toInt)
+    
+    def main(args: Array[String]) {
+
+    }
+}
