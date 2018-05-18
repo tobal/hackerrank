9a9092a92da41a30863cc8ab3de9c6569f05f46c
Tobal <tobal17@gmail.com>
2018-05-18 16:30:16 +0200

    artif-int/bot: add full solution for Bot Saves the Princess 2
    Signed-off-by: Tobal <tobal17@gmail.com>

--- artificial-intelligence/bot-building/bot-saves-princess-2.scala
+++ artificial-intelligence/bot-building/bot-saves-princess-2.scala
@@ -1,6 +1,30 @@
 object Solution {
+    
+    def determine(princess: (Int, Int), me: (Int, Int)): String = {
+        val horizontalDistance = princess._1 - me._1
+        val verticalDistance = princess._2 - me._2
+        if(horizontalDistance == 0) {
+            if(verticalDistance < 0) "LEFT" else "RIGHT"
+        }
+        else if(verticalDistance == 0) {
+            if(horizontalDistance < 0) "UP" else "DOWN"
+        }
+        else if(verticalDistance < 0) "LEFT" else "RIGHT"
+    }
+    
+    def findPrincess(grid: Array[String]): (Int, Int) = {
+        var princessLocation = (0, 0)
+        for((line, lineIndex) <- grid.zipWithIndex) {
+            if (line.indexOf('p') != -1) princessLocation = (lineIndex, line.indexOf('p'))
+        }
+        princessLocation
+    }
 
-    def nextMove(player:Int, r:Int, c:Int, grid:Array[String]) = ???
+    def nextMove(player:Int, r:Int, c:Int, grid:Array[String]) = {
+        val princessLoc = findPrincess(grid)
+        val myLoc = (r, c)
+        println(determine(princessLoc, myLoc))
+    }
 
     def main(args: Array[String]) = { 
         val n = Console.readInt
@@ -11,6 +35,6 @@ object Solution {
         for (i <- 0 until n) {
             grid.update(i, Console.readLine)
         }
-        nextMove(n, r, c, grid)
+        nextMove(n,r,c,grid)
     }
 }
