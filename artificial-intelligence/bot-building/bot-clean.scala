5822e2d784d868fd6538666c9238e6373697aafe
Tobal <tobal17@gmail.com>
2018-05-22 16:22:08 +0200

    artif-int/bot: add skeleton for Bot Clean
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ artificial-intelligence/bot-building/bot-clean.scala
@@ -0,0 +1,13 @@
+object Solution {
+    
+    def nextMove(pos:String, board:Array[String]) = ???
+
+    def main(args: Array[String]) = { 
+        val pos = Console.readLine
+        val board = new Array[String](5)
+        for (i <- 0 until 5) {
+            board.update(i, Console.readLine)
+        }
+    nextMove(pos, board)
+    }
+}
