0d4b87ebc612cd9ea80a6a30c9a0d432e56ab666
Tobal <tobal17@gmail.com>
2018-05-24 16:26:51 +0200

    artif-int/bot/bot-clean: add findBot function
    Signed-off-by: Tobal <tobal17@gmail.com>

--- artificial-intelligence/bot-building/bot-clean.scala
+++ artificial-intelligence/bot-building/bot-clean.scala
@@ -2,6 +2,14 @@ object Solution {
     
     def nextMove(pos:String, board:Array[String]) = ???
 
+    def findBot(board: Array[String]): (Int, Int) = {
+        var location = (0, 0)
+        for((line, lineIndex) <- grid.zipWithIndex) {
+            if (line.indexOf('b') != -1) location = (lineIndex, line.indexOf('b'))
+        }
+        location
+    }
+
     def main(args: Array[String]) = { 
         val pos = Console.readLine
         val board = new Array[String](5)
