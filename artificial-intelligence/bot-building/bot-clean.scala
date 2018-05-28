60739ba92812d9794fc4c9fb75ca58bd224b65d4
Tobal <tobal17@gmail.com>
2018-05-28 16:12:39 +0200

    artif-int/bot/bot-clean: automatically clean debris
    Signed-off-by: Tobal <tobal17@gmail.com>

--- artificial-intelligence/bot-building/bot-clean.scala
+++ artificial-intelligence/bot-building/bot-clean.scala
@@ -1,21 +1,24 @@
 object Solution {
-    
-    def nextMove(pos:String, board:Array[String]) = ???
 
-    def findBot(board: Array[String]): (Int, Int) = {
-        var location = (0, 0)
+    def nextMove(pos: String, board: Array[String]) = {
+        val botLoc = findBot(board)
+        if (botLoc.isEmpty) println("CLEAN")
+    }
+
+    def findBot(board: Array[String]): Option[(Int, Int)] = {
+        var location = None
         for((line, lineIndex) <- grid.zipWithIndex) {
-            if (line.indexOf('b') != -1) location = (lineIndex, line.indexOf('b'))
+            if (line.indexOf('b') != -1) location = Some(lineIndex, line.indexOf('b'))
         }
         location
     }
 
-    def main(args: Array[String]) = { 
+    def main(args: Array[String]) = {
         val pos = Console.readLine
         val board = new Array[String](5)
         for (i <- 0 until 5) {
             board.update(i, Console.readLine)
         }
-    nextMove(pos, board)
+        nextMove(pos, board)
     }
 }
