82be56b948b8e53c5fd7ff1acb38900a89fbb38e
Tobal <tobal17@gmail.com>
2018-06-12 16:17:29 +0200

    artif-int/bot: better solution for BotClean Large
    Signed-off-by: Tobal <tobal17@gmail.com>

--- artificial-intelligence/bot-building/bot-clean-large.scala
+++ artificial-intelligence/bot-building/bot-clean-large.scala
@@ -44,8 +44,9 @@ object Solution {
 
     def main(args: Array[String]) = {
         val pos = Console.readLine
-        val board = new Array[String](50)
-        for (i <- 0 until 50) {
+        val dimensions = Console.readLine.split(" ").toList.map(_.toInt)
+        val board = new Array[String](dimensions(0))
+        for (i <- 0 until dimensions(1)) {
             board.update(i, Console.readLine)
         }
         nextMove(pos, board)
