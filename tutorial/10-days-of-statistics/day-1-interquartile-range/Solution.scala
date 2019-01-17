81317e3920629b20e672b6463229b128ea2e7002
Tobal <tobal17@gmail.com>
2019-01-17 17:12:21 +0100

    10-days-of-statistics: experimenting with interquartile range
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ tutorial/10-days-of-statistics/day-1-interquartile-range/Solution.scala
@@ -0,0 +1,28 @@
+object Solution {
+
+    def calcMedian(l: List[Double]): Double = {
+        val (lower, upper) = l.splitAt(l.size / 2)
+        if (l.size % 2 == 0) (lower.last + upper.head) / 2 else upper.head
+    }
+
+    def calcQuartiles(l: List[Double]): (Double, Double, Double) = {
+        val m = calcMedian(l)
+        val (lower, upper) = l.splitAt(l.size / 2)
+        val lowMed = calcMedian(lower)
+        val upMed = if(lower.size == upper.size) calcMedian(upper)
+            else calcMedian(upper.tail)
+        (lowMed, m, upMed)
+    }
+
+    def main(args: Array[String]) {
+        readInt
+        var x = readLine.split(" ").toList.map(_.toDouble)
+        var f = readLine.split(" ").toList.map(_.toDouble)
+        println(x)
+        println(f)
+        println(x.zip(f))
+        println(for {
+            (value, freq) <- x.zip(f)
+        } yield List.fill(freq.toInt)(value))
+    }
+}
