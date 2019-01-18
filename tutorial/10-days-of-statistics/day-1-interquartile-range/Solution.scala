fd449898e2ae170f4c1a07e2fa52aa4658699386
Tobal <tobal17@gmail.com>
2019-01-18 11:16:40 +0100

    10-days-of-statistics: interquartile range example test passes
    Signed-off-by: Tobal <tobal17@gmail.com>

--- tutorial/10-days-of-statistics/day-1-interquartile-range/Solution.scala
+++ tutorial/10-days-of-statistics/day-1-interquartile-range/Solution.scala
@@ -1,5 +1,4 @@
 object Solution {
-
     def calcMedian(l: List[Double]): Double = {
         val (lower, upper) = l.splitAt(l.size / 2)
         if (l.size % 2 == 0) (lower.last + upper.head) / 2 else upper.head
@@ -18,11 +17,9 @@ object Solution {
         readInt
         var x = readLine.split(" ").toList.map(_.toDouble)
         var f = readLine.split(" ").toList.map(_.toDouble)
-        println(x)
-        println(f)
-        println(x.zip(f))
-        println(for {
+        val quartiles = calcQuartiles((for {
             (value, freq) <- x.zip(f)
-        } yield List.fill(freq.toInt)(value))
+        } yield List.fill(freq.toInt)(value)).flatten.sortWith(_ < _))
+        println(quartiles._3 - quartiles._1)
     }
 }
