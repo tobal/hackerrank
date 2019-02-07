6ab9f4263d8e83af03249818a2489cff3f42a890
Tobal <tobal17@gmail.com>
2019-02-07 16:34:37 +0100

    10-days-of-statistics/binomial-distribution-II: fix type and whitespace problems
    Signed-off-by: Tobal <tobal17@gmail.com>

--- tutorial/10-days-of-statistics/day-4-binomial-distribution-II/Solution.scala
+++ tutorial/10-days-of-statistics/day-4-binomial-distribution-II/Solution.scala
@@ -3,28 +3,28 @@ import scala.math.pow
 object Solution {
     def fact(n: Double): Double = {
         if (n == 0) 1
-        else n * fact(n-1)
+        else n * fact(n - 1)
     }
 
     def binomialDistribution(x: Double, n: Double, p: Double): Double = {
         (fact(n) / (fact(x) * fact(n - x))) * (pow(p, x) * ( pow((1 - p), (n - x)) ))
     }
 
-    def setScale(input: Double, scale: Int): Double = {
+    def setScale(input: List[Double], scale: Int): Double = {
         BigDecimal(input.sum).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble
     }
 
     def main(args: Array[String]) {
         val b = 0.0
-        val p = 1.12/1.00
+        val p = 1.12 / 1.00
         val n = 10.0
         val atMost2Rejects = for {
             i <- 0 until 3
         } yield binomialDistribution(i, n, p)
-        println(setScale(atMost2Rejects, 3))
+        println(setScale(atMost2Rejects.toList, 3))
         val atLeast2Rejects = for {
             i <- 2 until 11
         } yield binomialDistribution(i, n, p)
-        println(setScale(atLeast2Rejects, 3))
+        println(setScale(atLeast2Rejects.toList, 3))
     }
 }
